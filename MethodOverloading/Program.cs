cab3de48d5f64cb06ec69a54a799b85a667d8587
Matthew Yglesias <mdywrk@gmail.com>
2026-03-23 21:09:00 -0400

    Method overloaded.

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
         }
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        static string Add(int a, int b, bool includeCurrency)
+        {
+            var sum = a + b;
+            if (includeCurrency)
+            {
+                if (sum == 1)
+                {
+                    return sum + " dollar";
+                }
+                else
+                {
+                    return sum + " dollars";
+                }
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+
     }
 }
